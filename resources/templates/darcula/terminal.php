3fe658fdad4d3d80c4cd3206d2ff0efe98fe5905
chrisdevelops <crowles@tutanota.de>
2018-10-06 16:02:46 +0100

    remove duplication and correct the stylesheet for the default theme

--- resources/templates/darcula/terminal.php
+++ resources/templates/darcula/terminal.php
@@ -1,32 +1,3 @@
-<?php
-
-/** @var $this \CaptainSpain\WebTerminal\Terminal */
-
-function makeInputLine($data, $fromData = true)
-{
-    $command = '';
-    $response = '';
-    $operator = $data['isSudo'] ? '#' : '$';
-    if ($fromData) {
-        $command = $data['command'];
-        if (!contains('cd ', $command)) {
-            $command .= '<br />';
-        }
-        $command = '<span class="command-executed">' . $command . '</span>';
-        $responseData = $data['response'] ?? [];
-        $response = '';
-        foreach ($responseData as $line) {
-            $line = $line === '' ? '&nbsp;' : htmlentities($line);
-            $response .= '<span class="new-line" style="display:block">' . $line . '</span>';
-        }
-        $response = '<span class="response">' . $response . '</span>';
-    }
-
-    return <<<HTML
-<span class="inputLine"><span class="user">{$data['user']}</span><span class="atHost">@</span><span class="host">{$data['host']}</span>:<span class="root">{$data['root']} </span><span class="operator">{$operator}</span>&nbsp;</span>{$command}{$response}
-HTML;
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -41,6 +12,12 @@ HTML;
 <div id="console">
     <input type="hidden" id="history" value="<?= $this->getHistoryFromData(true); ?>">
     <span class="welcome">
+        ██╗    ██╗███████╗██████╗       ████████╗███████╗██████╗ ███╗   ███╗██╗███╗   ██╗ █████╗ ██╗
+        ██║    ██║██╔════╝██╔══██╗      ╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗██║
+        ██║ █╗ ██║█████╗  ██████╔╝█████╗   ██║   █████╗  ██████╔╝██╔████╔██║██║██╔██╗ ██║███████║██║
+        ██║███╗██║██╔══╝  ██╔══██╗╚════╝   ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║██║╚██╗██║██╔══██║██║
+        ╚███╔███╔╝███████╗██████╔╝         ██║   ███████╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║██║  ██║███████╗
+         ╚══╝╚══╝ ╚══════╝╚═════╝          ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝
     </span><br />
     <?php foreach ($this->data as $data): ?>
         <?= makeInputLine($data); ?>
--- src/helpers.php
+++ src/helpers.php
@@ -36,6 +36,41 @@ if (!function_exists('asset')) {
     }
 }
 
+/**
+ * @param $data
+ * @param bool $fromData
+ * @return string
+ */
+function makeInputLine($data, $fromData = true) : string
+{
+    $command = '';
+    $response = '';
+    $operator = $data['isSudo'] ? '#' : '$';
+    if ($fromData) {
+        $command = $data['command'];
+        if (!contains('cd ', $command)) {
+            $command .= '<br />';
+        }
+        $command = '<span class="command-executed">' . $command . '</span>';
+        $responseData = $data['response'] ?? [];
+        $response = '';
+        foreach ($responseData as $line) {
+            $line = $line === '' ? '&nbsp;' : htmlentities($line);
+            $response .= '<span class="new-line" style="display:block">' . $line . '</span>';
+        }
+        $response = '<span class="response">' . $response . '</span>';
+    }
+
+    return <<<HTML
+<span class="inputLine"><span class="user">{$data['user']}</span><span class="atHost">@</span><span class="host">{$data['host']}</span>:<span class="root">{$data['root']}</span><span class="operator">{$operator}</span>&nbsp;</span>{$command}{$response}
+HTML;
+}
+
+/**
+ * @param $needle
+ * @param $haystack
+ * @return bool
+ */
 function contains($needle, $haystack)
 {
     return strpos($haystack, $needle) !== false;
